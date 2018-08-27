2a5f76c8f4ed1aa71df24a45c68ce301532f81af
Steeve Joseph <steevejoseph@knights.ucf.edu>
2018-08-27 07:23:33 -0400

    fix another typo

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express  = require("express"),
     app      = express();
 
 
-mongoose.connect('mongodb://team7:ABC123@ds133152.mlab.com:33152/contact-manager', use);
+mongoose.connect('mongodb://team7:ABC123@ds133152.mlab.com:33152/contact-manager');
 
 app.get("/", function(req, res){
 	res.render('splash.ejs');
