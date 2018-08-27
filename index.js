62b9787a7cbad145d1c8949e697f1491fb7bc05a
Steeve Joseph <steevejoseph@knights.ucf.edu>
2018-08-27 07:19:30 -0400

    add mlab (prod) DB URL

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-var express = require("express"),
-    app     = express();
+var express  = require("express"),
+	mongoose = require("mogoose"),
+    app      = express(),
 
+
+mongoose.connect('mongodb://<team7>:<ABC123>@ds133152.mlab.com:33152/contact-manager');
 
 app.get("/", function(req, res){
 	res.render('splash.ejs');
