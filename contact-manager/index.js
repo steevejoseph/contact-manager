42c823a96b5b16993cde767de04fd2ebcef46b17
Steeve Joseph <steevejoseph@knights.ucf.edu>
2018-08-27 07:00:08 -0400

    change index.js to debug heroku

---
+++ contact-manager/index.js
@@ -0,0 +1,13 @@
+var express = require("express"),
+    app     = express();
+
+
+app.get("*", function(req, res){
+	res.render('splash.ejs');
+});
+
+
+
+app.listen(9000, process.env.IP, function(){
+	console.log('Server running!');
+})
