f09a75ea41192e2b7a09b70ff58cea3be95084b0
abeSanchez <abraham5001@aol.com>
2018-08-29 14:29:06 -0400

    Because im annoying

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 var express  = require("express"),
-	mongoose = require("mongoose"),
+    mongoose = require("mongoose"),
     app      = express();
-
 
 mongoose.connect('mongodb://team7:ABC123@ds133152.mlab.com:33152/contact-manager');
 
 app.get("/", function(req, res){
 	res.render('splash.ejs');
 });
-
-
 
 app.listen(process.env.PORT, process.env.IP, function(){
 	console.log('Server running!');
