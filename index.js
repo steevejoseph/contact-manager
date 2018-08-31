9aaa77cb7373d2fca33e2352b28ba66e47989a9f
alittlebirdie00 <35277534+alittlebirdie00@users.noreply.github.com>
2018-08-31 06:52:08 -0400

    Setting up for Authentication
    Included some dependencies and starting setting up Passport

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-var express  = require("express"),
-	mongoose = require("mongoose"),
-	app      = express();
+// Include dependencies
+var express 				= require("express"),
+	mongoose				= require("mongoose"),
+	passport				= require("passport"),
+	bodyParser				= require("body-parser"),
+	LocalStrategy			= require("passport-local"),
+	passportLocalMongoose	= require("passport-local-mongoose"),
+	app     				= express();
 
 // Include the Schemas/Models
 var	Contact  = require("./models/contact.js"),
@@ -8,6 +13,29 @@ var	Contact  = require("./models/contact.js"),
 
 // Connect to URL set in env variables.
 mongoose.connect('mongodb://team7:ABC123@ds133152.mlab.com:33152/contact-manager', {useNewUrlParser: true});
+
+app.set("view engine", "ejs");
+
+// Add the Express Session In
+// call .use on express object (app var) on something we're requiring, 
+// and executing, with options
+app.use(require("express-session")({
+    // this secret will be used to 
+    // encode (encrypt) and decode(decrypt) the sessions.
+    secret:"I love you",
+    resave: false,
+    saveUninitialized: false
+}));
+
+// Tell Express to ---- USE PASSPORT-----
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Two more lines before we can start working on the routes
+// Responsible for encoding data and putting it back into the session
+passport.serializeUser(User.serializeUser());
+// Responsible for reading the session, taking data from the session and unencoding it
+passport.deserializeUser(User.deserializeUser());
 
 app.use(express.static(__dirname + '/public'));
 
