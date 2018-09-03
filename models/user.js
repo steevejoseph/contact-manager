12345597bfc27d81374edb8c0bcd95bda0131a8d
alittlebirdie00 <35277534+alittlebirdie00@users.noreply.github.com>
2018-09-03 16:57:07 -0400

    Add files via upload

---
+++ models/user.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose'),
+    passportLocalMongoose = require("passport-local-mongoose");
+
+
+var userSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: {type:String, default: 'Last Name'},
+
+    // email validation will be done on frontend.
+    email: String,
+    username: String,
+
+    // hashed password
+    password: String,
+    birthday: Date,
+    contacts: [{type:mongoose.Schema.Types.ObjectId, ref:'Contact'}]
+});
+
+// After we've defined the schema, use passport
+// This goes ahead and adds important method that come with the passport
+// package, that we need to use inorder to have user Authentication
+// such as serialize() and deserialize()
+userSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', userSchema);
