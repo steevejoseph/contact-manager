da8aad31fd5e81e39ef0996ff0b25a7d454910d3
Jonathan Wyatt Feck <jwf1112@knights.ucf.edu>
2018-09-07 18:35:35 +0000

    implimented api for searching and adding a contact

--- api/contacts.js
+++ api/contacts.js
@@ -7,38 +7,32 @@ const mongoose = require('mongoose');
 const Contact = require('../models/contact.js');
 const user = require('../models/user.js');
 mongoose.connect('mongodb://team7:ABC123@ds133152.mlab.com:33152/contact-manager', {useNewUrlParser: true});
-//calls to this .js should already be at '/api' so here is just handeling the '/' after '/api'
-router.get('/api/users/', (req, res, next) => {
-    res.status(200).json({
-        message: 'handeling get requests to api/users'
-    });
-    
-});
 
-router.post('/api/contacts', (req, res, next) => {
-    res.status(200).json({
-        message : 'handeling post requests to /api/contacts'
-    });
-});
 
-router.get('/api/users/:id/search/', (req, res, next) => {
-    var id = req.params.id;
-    var query = req.body.query;
+router.get('/api/users/:id/search/:query', (req, res, next) => {
+    var query = req.params.query;
+    var userID = req.params.id;
+    Contact.find({firstName:query, user:userID}, function(err, contacts) {
+        if(err) {
+            console.log(err);
+        }
+        else {
+            for(var i = 0; i < contacts.length; i++) {
+                console.log(contacts[i]);
+            }
+            {contacts:contacts};
+        }
     
     
     res.status(200).json({
-        message : 'search for contacts with id: ' + id
+        message : 'search for ' + query,
+        contactsList: contacts
     });
     
 });
+});
 
-router.get('/api/ids', (req, res, next) => {
-    
-})
-
-router.post('/api/:id/addcontact', (req, res, next) => {
-    
-    
+router.post('/api/users/:id/addcontact', (req, res, next) => {
     var name = req.body.firstName;
     
     const contact = new Contact({
@@ -51,7 +45,7 @@ router.post('/api/:id/addcontact', (req, res, next) => {
         user: req.params.id
     });
     contact.save();
-    res.status(200).json({
+    res.status(201).json({
         message: 'handeling post requests for /api/users/:id/addcontact',
         createdContact: contact
     });
